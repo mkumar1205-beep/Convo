4e3047fb8fa73650fa5b76bc6775fc5ae9c04f55
Mkumar <manyakumar1205@gmail.com>
2026-04-28 22:37:29 +0530

    Add chat control

---
+++ backend/Controller/chatController.js
@@ -0,0 +1,98 @@
+const Message = require('../models/chat')
+const User = require('../models/user')
+const sendMessage = async (req, res) => {
+  try {
+    const { receiverId, text } = req.body
+
+    if (!receiverId || !text) {
+      return res.status(400).json({
+      success: false,
+      message: 'Receiver and text are required'
+     })
+    }
+
+    const receiver = await User.findById(receiverId)
+    if (!receiver) {
+      return res.status(404).json({
+        success: false,
+        message: 'Receiver not found'
+      })
+    }
+    const message = await Message.create({
+      sender: req.user._id,
+      receiver: receiverId,
+      text
+    })
+
+    res.status(201).json({
+      success: true,
+      message
+    })
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    })
+  }
+}
+
+const getMessages = async (req, res) => {
+  try {
+    const myId = req.user._id
+    const otherUserId = req.params.userId
+
+    const messages = await Message.find({
+      $or: [
+        { sender: myId, receiver: otherUserId },
+        { sender: otherUserId, receiver: myId }
+      ]
+    })
+    .sort({ createdAt: 1 })
+    .populate('sender', 'username')
+    .populate('receiver', 'username')
+
+    res.status(200).json({
+      success: true,
+      messages
+    })
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    })
+  }
+}
+
+const deleteMessage = async (req, res) => {
+  try {
+    const message = await Message.findById(req.params.id)
+
+    if (!message) {
+      return res.status(404).json({ 
+        success: false,
+        message: 'Message not found' })
+    }
+
+    // only sender can delete
+    if (message.sender.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ 
+        success: false,
+        message: 'Not allowed' })
+    }
+
+    await message.deleteOne()
+
+    res.status(200).json({
+      success: true,
+      message: 'Message deleted'
+    })
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    })
+  }
+}
