1e74756d278d103f12950c2dabb6703f115246d9
Mkumar <manyakumar1205@gmail.com>
2026-04-29 06:22:28 +0530

    Add server and db

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const app = express()
+
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connected"))
+  .catch(err => console.log(err))
+
+app.use('/api/auth', require('./routes/authRoutes'))
+app.use('/api/messages', require('./routes/messageRoutes'))
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('DB connected'))
+  .catch(err => console.log(err))
+
+const PORT = 5000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
